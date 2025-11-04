f4d286154d338ce399fe214fbff6ba34bd442f52
mayascodes <m244.sc@gmail.com>
2025-11-04 17:55:21 +0000

    Add files via upload

---
+++ 11operatorsprac.py
@@ -0,0 +1,17 @@
+#print(int(7.8) == 7 or 5 > 10) = True
+#print(bool(0), bool("Python"), bool([])) = False, True, False
+"""
+x = 4.2
+y = isinstance(x, int)
+print(y) = False
+"""
+#print(float(3 + int(2.9) % 2)) = 3.0
+"""
+x = 5 + True * 2 - False
+print(x) 7
+"""
+"""
+a = 8
+b = 3
+print(a // b * b + a % b) = 8
+"""
